cc05163cd889d5fdbbfecfc135f2d5d0bbec6510
nakamurj23 <nakamurj23@up.edu>
2025-10-14 21:39:32 -0700

    completed flexbox froggy

--- hw1/02-palindrome.js
+++ hw1/02-palindrome.js
@@ -1,3 +1,10 @@
-const elem = document.querySelector('input');
+const elem = document.querySelector("input");
 
-elem.addEventListener('input', handleInput);
+elem.addEventListener("input", handleInput);
+
+function handleInput() {
+  console.log(elem.toString().length);
+  if (elem.toString().length == 1) {
+    console.log("palindrome");
+  }
+}
