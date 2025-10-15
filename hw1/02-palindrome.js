8c9484f81ce3e28ae984fffa9d00672698eca7a2
nakamurj23 <78463390+nakamurj23@users.noreply.github.com>
2025-10-14 22:33:27 -0700

    completed palindrom script

--- hw1/02-palindrome.js
+++ hw1/02-palindrome.js
@@ -3,8 +3,32 @@ const elem = document.querySelector("input");
 elem.addEventListener("input", handleInput);
 
 function handleInput() {
-  console.log(elem.toString().length);
-  if (elem.toString().length == 1) {
-    console.log("palindrome");
+  const input = elem.value;
+  const array = input.toString().split("");
+  const output = document.getElementById("output");
+
+  // check that input is non-empty
+  if (array.length > 0) {
+    // declare boolean
+    let isPalindrome = true;
+    // loop through array checking each side
+    for (let i = 0; i < array.length / 2; i++) {
+      // if mismatch set boolean to false and break
+      if (array[i] != array[array.length - 1 - i]) {
+        isPalindrome = false;
+        break;
+      }
+    }
+    // check boolean
+    if (isPalindrome) {
+      output.innerHTML = "Yes";
+      output.style.color = "green";
+    } else {
+      output.innerHTML = "No";
+      output.style.color = "red";
+    }
+  } else {
+    // if length is zero display nothing
+    output.innerHTML = "";
   }
 }
